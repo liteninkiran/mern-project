b6ddc6b1c1dbc76f49e72a09e11652bf2946d837
Kiran Anand <liteninkiran@hotmail.com>
2021-09-12 15:46:50 +0100

    Model/controller

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { getPosts } from '../contollers/posts.js';
+import { createPost } from '../contollers/posts.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('This works');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
