d6356b5b2d769c3e2b1887164da06b1d8e3ce5fa
Kiran Anand <liteninkiran@hotmail.com>
2021-09-13 18:09:36 +0100

    Tidy up

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -43,7 +43,7 @@ const Post = ({ post, setCurrentId }) => {
 
             {/* Post Message */}
             <CardContent>
-                <Typography gutterBottom>{ post.message }</Typography>
+                <Typography variant="body2">{ post.message }</Typography>
             </CardContent>
 
             {/* Like / Delete */}
