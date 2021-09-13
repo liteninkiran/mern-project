428906b633d11626a9afa1c7ca9273204e6d728b
Kiran Anand <liteninkiran@hotmail.com>
2021-09-13 19:00:36 +0100

    Environment variables

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jsm-user:pa55word@cluster0.ppmus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-.connect(CONNECTION_URL, {
+.connect(process.env.CONNECTION_URL, {
     // Do stuff
 })
 .then(() => {
