8c52920e34aec6c269a70b18d506b612587a89ef
Kiran Anand <liteninkiran@hotmail.com>
2021-09-13 20:45:29 +0100

    Move navbar and home page out of App.js

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Auth = () => {
+
+    const state = null;
+
+    return (
+        <div>
+            Auth Page
+        </div>
+    )
+}
+
+export default Auth
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../../actions/posts';
+
+import Posts from '../../components/Posts/Posts';
+import Form from '../../components/Form/Form';
+
+import useStyles from '../../styles';
+
+const Home = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const [currentId, setCurrentId] = useState(null);
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form setCurrentId={setCurrentId} currentId={currentId} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    );
+}
+
+export default Home;
