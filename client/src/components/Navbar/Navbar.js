2a58143f13af34556a357bfb1476ace394fc8bea
Kiran Anand <liteninkiran@hotmail.com>
2021-09-14 11:35:06 +0100

    Logout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { AppBar, Typography, Avatar, Toolbar, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import memories from '../../images/memories.png';
 import useStyles from './styles';
@@ -9,34 +11,57 @@ import useStyles from './styles';
 const Navbar = () => {
     const classes = useStyles();
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
-    console.log(user);
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+        history.push('/');
+        setUser(null);
+    };
 
     useEffect(() => {
-        const token = user?.token;
-
         setUser(JSON.parse(localStorage.getItem('profile')));
-    }, []);
+    }, [location]);
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
+
+            {/* Title & App Icon */}
             <div className={classes.brandContainer}>
+
+                {/* Title */}
                 <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
                     Memories
                 </Typography>
+
+                {/* App Icon */}
                 <img className={classes.image} src={memories} alt="icon" height="60" />
+
             </div>
 
+            {/* User Details / Login */}
             <Toolbar className={classes.toolbar}>
                 {
                     user ? (
                         <div className={classes.profile}>
+
+                            {/* User Icon */}
                             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+
+                            {/* User Name */}
                             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+
+                            {/* Logout Button */}
+                            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+
                         </div>
                     ) : (
+
+                        // Login Button
                         <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+
                     )
                 }
 
