be289cc3ca4e9c7606131dae6e298a189f7fb5c2
Kiran Anand <liteninkiran@hotmail.com>
2021-09-16 11:10:45 +0100

    Hide edit / delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -38,6 +38,10 @@ const Post = ({ post, setCurrentId }) => {
         return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
     }
 
+    const handleDeletePost = () => {
+        dispatch(deletePost(post._id));
+    };
+
     return (
 
         // Card
@@ -53,11 +57,13 @@ const Post = ({ post, setCurrentId }) => {
             </div>
 
             {/* Edit Ellipsis */}
-            <div className={ classes.overlay2 }>
-                <Button style={ { color: 'white' } } size="small" onClick={ () => setCurrentId(post._id) }>
-                    <MoreHorizIcon fontSize="medium" />
-                </Button>
-            </div>
+            {( user?.result?.googleId === post.creator || user?.result?._id === post.creator) && (
+                <div className={ classes.overlay2 }>
+                    <Button style={ { color: 'white' } } size="small" onClick={ () => setCurrentId(post._id) }>
+                        <MoreHorizIcon fontSize="medium" />
+                    </Button>
+                </div>
+            )}
 
             {/* Tags */}
             <div className={ classes.details }>
@@ -81,10 +87,11 @@ const Post = ({ post, setCurrentId }) => {
                 </Button>
 
                 {/* Delete */}
-                <Button size="small" color="primary" onClick={ () => dispatch(deletePost(post._id)) }>
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                </Button>
+                {( user?.result?.googleId === post.creator || user?.result?._id === post.creator) && (
+                    <Button size="small" color="primary" onClick={ handleDeletePost }>
+                        <DeleteIcon fontSize="small" />Delete
+                    </Button>
+                )}
 
             </CardActions>
 
