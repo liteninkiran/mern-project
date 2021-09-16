bd3a72b95bc53fba9549c29d5cd5346d4054233c
Kiran Anand <liteninkiran@hotmail.com>
2021-09-16 19:28:35 +0100

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
 
@@ -14,15 +15,18 @@ const App = () => {
 
         <BrowserRouter>
 
-            <Container maxwidth="lg">
+            <Container maxwidth="xl">
 
                 {/* Navbar */}
                 <Navbar />
                 
                 {/* Home / Auth */}
                 <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/" />)} />
+                    <Route path="/" exact component={() => <Redirect to='/posts' />} />
+                    <Route path="/posts" exact component={Home} />
+                    <Route path="/posts/search" exact component={Home} />
+                    <Route path="/posts/:id" component={PostDetails} />
+                    <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
                 </Switch>
 
             </Container>
