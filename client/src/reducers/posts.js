1d336bc95eb84c47360dddd84ec0c71d83a2e629
Kiran Anand <liteninkiran@hotmail.com>
2021-09-16 23:41:51 +0100

    Add actions

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,13 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 
 const reducer = (posts = [], action) => {
     switch (action.type) {
-        case FETCH_ALL: return action.payload;
-        case CREATE   : return [...posts, action.payload];
-        case UPDATE   : return posts.map((post) => post._id === action.payload._id ? action.payload : post );
-        case DELETE   : return posts.filter((post) => post._id !== action.payload);
-        default       : return posts;
+        case FETCH_ALL       : return action.payload;
+        case FETCH_BY_SEARCH : return action.payload; // { ...state, posts: action.payload.data };
+        case CREATE          : return [...posts, action.payload];
+        case UPDATE          : return posts.map((post) => post._id === action.payload._id ? action.payload : post );
+        case DELETE          : return posts.filter((post) => post._id !== action.payload);
+        default              : return posts;
     }
 };
 
