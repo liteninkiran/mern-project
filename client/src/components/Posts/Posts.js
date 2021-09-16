3a9c15d1ff7b1457b4c887a9239cb8604bbb548d
Kiran Anand <liteninkiran@hotmail.com>
2021-09-16 19:19:18 +0100

    Removing icon

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 const Posts = ({setCurrentId}) => {
     const posts = useSelector((state) => state.posts);
