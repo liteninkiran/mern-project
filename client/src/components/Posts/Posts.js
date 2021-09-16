e0352f8846dbbedd70832c318d62241068129f4b
Kiran Anand <liteninkiran@hotmail.com>
2021-09-16 11:51:37 +0100

    Remove spinner

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -10,7 +10,7 @@ const Posts = ({setCurrentId}) => {
 
     return (
 
-        !posts.length ? <CircularProgress /> : (
+        !posts.length ? <></> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 { posts.map((post) => (
                     <Grid key={ post._id } item xs={12} sm={6}>
